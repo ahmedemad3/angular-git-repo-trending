bb1a1316e46720b6e9bef605dd7f31e67b8ed2f9
ahmedemad3 <ahmedemad3@gmail.com>
2022-01-03 00:06:12 +0200

    adding angular trending repo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeveloperListComponent } from './developer-list/developer-list.component';
+import { RepoListComponent } from './repo-list/repo-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'repositories', component: RepoListComponent },
+  { path: 'developers', component: DeveloperListComponent },
+  { path: '', component: RepoListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './repo/repo.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { RepoListComponent } from './repo-list/repo-list.component';
+import { DeveloperListComponent } from './developer-list/developer-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent,
+    DeveloperComponent,
+    RepoListComponent,
+    DeveloperListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
